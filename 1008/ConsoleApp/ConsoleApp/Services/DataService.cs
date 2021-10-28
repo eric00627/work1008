d11befaef4f5209cda18361add2071b77be9f608
user1 <C107152301@nkust.edu.tw>
2021-10-29 00:00:20 +0800

    renew the code in 1028

---
+++ 1008/ConsoleApp/ConsoleApp/Services/DataService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Services
+{
+    public class DataService {
+        private string _connectionString = @"Server=DESKTOP-FP5B1VH\SQLEXPRESS;Database=NKUST;Trusted_Connection=True";
+
+        public void GetMedical_institution()
+        {
+            var cn = new SqlConnection(_connectionString);
+            cn.Open();
+            SqlCommand command = new SqlCommand("Select * from Medical_institution", cn);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var id = reader["十碼章"];
+                var name = reader["醫院名稱"];
+                var Administrative_District = reader["行政區"];
+                var address = reader["地址"];
+                var telephone = reader["連絡電話"];
+
+            }
+            cn.Close();
+        }
+
+
+    }
+}
